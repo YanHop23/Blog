f5a921e31375da4c6ca0effd8fe659a60e841d64
YanHop23 <olegpolothevets@gmail.com>
2025-02-14 15:53:53 +0200

    create model user

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,42 @@
-import express from "express"
-import jwt from "jsonwebtoken"
-import mongoose from "mongoose"
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
+const app = express();
+dotenv.config();
 
+mongoose
+    .connect(process.env.MONGODB_URI, {})
+    .then(() => console.log('MongoDB Connected'))
+    .catch((err) => console.log('MongoDB Error', err));
 
-const app = express()
-mongoose.connect(process.env.MONGODB_URI, {})
-app.use(express.json())
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
-app.post("/auth/login", (req, res) => {
-    console.log(req)
+app.post('/auth/login', (req, res) => {
+    console.log(req);
 
-    const token = jwt.sign({
-        email: req.body.email,
-        fullName: "Олежка",
-    },
-    "secret"
-    )
+    const token = jwt.sign(
+        {
+            email: req.body.email,
+            fullName: 'Олежка',
+        },
+        'secret'
+    );
 
     res.json({
-        status: "success",
+        status: 'success',
         token: token,
-    })
-})
+    });
+});
 
 app.listen(4444, (error) => {
     if (error) {
-        return console.error(error)
+        return console.error(error);
     }
 
-    console.log("Server started on port 4444")
-
-})
-
+    console.log('Server started on port 4444');
+});
